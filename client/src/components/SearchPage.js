390e3aa8075c5b8069d2b2de31eca446f8936d5a
Tracey Saada <tracey.saada@outlook.com>
2021-04-18 14:27:40 +0100

    added function for search pages

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,13 +1,63 @@
 import React from 'react'
 import Jumbotron from "../components/Jumbotron";
+import { Col, Row, Container } from "../components/Grid";
+import UserSearch from "./UserSearch";
+import API from "../utils/ApiCalls"
 
-function SearchPage () {
+class SearchPage extends React.Component {
+  state = {
+    books: [],
+    q: "",
+  };
+  getBooksFromGoogleAPI = () => {
+      console.log("searching", this.state.q)
+    API.getGoogleBooksSearch(this.state.q).then(results => console.log(results))
+  };
+  handleUserInput = e => {
+    this.setState({
+        q: e.target.value
+    })
+  };
+  saveBookFromDatabase = (bookObject) => {
+    return;
+  };
+  handleSearch = (event) => {
+      event.preventDefault();
+      console.log("Button blicked")
+      this.getBooksFromGoogleAPI();
+  };
+  render() {
     return (
+      <>
+         <Container fluid>
+        <Row>
+          <Col size="md-12">
         <Jumbotron>
+      
         <h1>Search For Books</h1>
         <p>Search Books you like & Save your Favourite books</p>
         </Jumbotron>
-    )
+        </Col>
+        <UserSearch
+          q={this.state.q}
+          handleSearch={this.handleSearch}
+          handleUserInput={this.handleUserInput}
+          />
+        </Row>
+        <Row>
+          <Col size="md-10 md-offset-1">
+            <article>
+              <h1>Book Title:</h1>
+              <p>BODY</p>
+            </article>
+          </Col>
+          <button className="btn">View</button>
+            <button className="btn">Save Favourite</button> 
+          
+        </Row>
+        </Container>
+      </>
+    );
+  }
 }
-
-export default SearchPage; 
+export default SearchPage;
