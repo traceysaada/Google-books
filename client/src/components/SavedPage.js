fb71392ddaddabecd1f242daae9cf6855407d554
Tracey Saada <tracey.saada@outlook.com>
2021-04-18 14:27:19 +0100

    added functions for saved books

--- client/src/components/SavedPage.js
+++ client/src/components/SavedPage.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import Jumbotron from "../components/Jumbotron";
+import { Col, Row, Container } from "../components/Grid";
 
-function SavedPage () {
-    return (
+class SavedBooks extends React.Component {
+state = {
+    books: [],
+  };
+  componentDidMount() {
+    return;
+  }
+  getDatabaseBooks = () => {
+    return;
+  };
+  deleteBookFromDatabase = (bookId) => {
+    return;
+  };
+  render() {
+      return (
+    <Container fluid>
+        <Row>
+          <Col size="md-12">
         <Jumbotron>
         <h1>Saved Page</h1>
         <p>View or Remove your Saved Books</p>
         </Jumbotron>
-    )
+        </Col>
+        </Row>
+        <Row>
+          <Col size="md-10 md-offset-1">
+            <article>
+              <h1>Book Title:</h1>
+              <p>BODY</p>
+            </article>
+          </Col>
+          <button className="btn">View</button>
+            <button className="btn btn-danger">Remove</button> 
+          
+        </Row>
+        </Container>
+      )
+  }
 }
+export default SavedBooks;
 
-export default SavedPage; 
