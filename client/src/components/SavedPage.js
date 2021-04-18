f664971f31c0b78fcccf476f5eb8510ec16fd1df
Tracey Saada <tracey.saada@outlook.com>
2021-04-18 12:07:23 +0100

    added jumbotron

--- client/src/components/SavedPage.js
+++ client/src/components/SavedPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Jumbotron from "../components/Jumbotron";
 
 function SavedPage () {
     return (
+        <Jumbotron>
         <h1>Saved Page</h1>
+        <p>View or Remove your Saved Books</p>
+        </Jumbotron>
     )
 }
 
