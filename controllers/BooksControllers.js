a6a095b0dc80ed1dc950629dcd135b416ba12169
Tracey Saada <tracey.saada@outlook.com>
2021-04-19 18:27:44 +0100

    change to correct wording on dbBooks

--- controllers/BooksControllers.js
+++ controllers/BooksControllers.js
@@ -2,32 +2,32 @@ const dbBooks = require("../models/bookSchema");
 
 module.exports = {
     findAll: function(req, res) {
-      dbBooks.Book
+      dbBooks
         .find(req.query)
         .sort({ date: -1 })
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     findById: function(req, res) {
-      dbBooks.Book
+      dbBooks
         .findById(req.params.id)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
-      dbBooks.Book
+      dbBooks
         .create(req.body)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     update: function(req, res) {
-      dbBooks.Book
+      dbBooks
         .findOneAndUpdate({ _id: req.params.id }, req.body)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     remove: function(req, res) {
-      dbBooks.Book
+      dbBooks
         .findById({ _id: req.params.id })
         .then(dbModel => dbModel.remove())
         .then(dbModel => res.json(dbModel))
