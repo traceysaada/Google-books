618ea8d1c5d411ac65685738a182ed51693a9b1f
Tracey Saada <tracey.saada@outlook.com>
2021-04-19 18:28:39 +0100

    added googleId

--- models/bookSchema.js
+++ models/bookSchema.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
+    googleId: {type: String},
     title: {type: String, required: true},
     authors: { type: [String], required: true},
     description: {type: String, required: true},
-    Image: {type: String, required: true},
+    image: {type: String, required: true},
     link: {type: String, required: true}
 })
 
