f4c3ac6cb31a84a58b35d6c0524e65b84c44f77b
Tracey Saada <tracey.saada@outlook.com>
2021-04-19 18:24:37 +0100

    added cards file to components

---
+++ client/src/components/BooksCards.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function BooksCards({title, authors, description, image, link, Button}){
+    return (
+    <>
+    <p>{title}</p>
+    <Button/>
+    </>
+    )
+}
+
+export default BooksCards
