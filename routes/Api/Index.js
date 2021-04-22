b9409146ea31335431829723975687d60ffb007c
Tracey Saada <tracey.saada@outlook.com>
2021-04-22 18:23:18 +0100

    Remove erroneous index file

--- routes/Api/Index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const bookRoutes = require("./Books");
-const googleBooks = require("./GoogleBooks")
-
-// Post routes
-router.use("/books", bookRoutes);
-router.use("/googlebooks", googleBooks);
-
-module.exports = router;
