448d1c4b4cae0e0e68e91c5a1ebf4af25acfa147
Tracey Saada <tracey.saada@outlook.com>
2021-04-23 20:27:03 +0100

    added pseudo for incomplete work

--- client/src/components/BooksCards.js
+++ client/src/components/BooksCards.js
@@ -8,6 +8,7 @@ function BooksCards({title, authors, description, image, link, Button}){
         <Button />
         <button target="_blank" href={link} rel="noreferrer">View</button>
         </>
+        // still need to work on getting the view data button to work
     )
 }
 export default BooksCards
